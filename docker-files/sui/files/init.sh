760bf01876ad5604c47931b7adc8d582f493332d
Akshay <akshay.ap95@gmail.com>
2023-01-29 16:26:02 +0100

    Add sui setup and docker compose

---
+++ docker-files/sui/files/init.sh
@@ -0,0 +1,3 @@
+sui genesis
+# gas_address=$(sui client gas | sed -n '3 p' | cut -d' ' -f2,5)
+sui client transfer-sui --amount 1000000 --to 0xa7d262f7599441aef3499d7f6b5b69ef35610259 --sui-coin-object-id $(sui client gas | sed -n '3 p' | cut -d' ' -f2,5) --gas-budget 1000# apt-get update
